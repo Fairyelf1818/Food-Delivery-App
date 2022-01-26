00c94357424c9b400c240a13ab758268af21568e
Fairyelf1818 <angeliqueow12@gmail.com>
2022-01-25 19:44:27 -0500

    Somewhat complete

---
+++ www/js/script.js
@@ -0,0 +1,20 @@
+document.getElementById("getPosition").addEventListener("click", getPosition);
+
+function getPosition() {
+   var options = {
+      enableHighAccuracy: true,
+      maximumAge: 3600000
+   }
+   var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+
+   function onSuccess(position) {
+       document.getElementById("line1").value="LA " + position.coords.latitude ;
+       document.getElementById("city").value="LO " + position.coords.longitude ;
+       document.getElementById("parish").value="AC " + position.coords.accuracy ;
+      alert("Successful! Please Enter your name and phone number");
+   };
+
+   function onError(error) {
+      alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+   }
+}
